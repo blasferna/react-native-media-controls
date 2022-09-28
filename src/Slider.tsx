36c4373e8f695be246b6edf695b6ac68ee9a00cf
blasferna <blasferna@gmail.com>
2022-09-28 14:12:36 -0400

    Unused import removed

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TouchableOpacity, View, Text, Image, ViewStyle, StyleSheet } from "react-native";
+import { TouchableOpacity, View, Text, Image, ViewStyle } from "react-native";
 import {Slider as RNSlider} from '@miblanchard/react-native-slider';
 import styles from "./MediaControls.style";
 import { humanizeVideoDuration } from "./utils";
